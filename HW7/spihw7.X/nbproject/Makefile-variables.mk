ee736f89003406449bc60306d0eee4db80fc7542
Simon-tj <53496703+Simon-tj@users.noreply.github.com>
2021-04-23 10:31:09 -0500

    hw7 start

---
+++ HW7/spihw7.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=spihw7.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/spihw7.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=spihw7.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/spihw7.x.tar
